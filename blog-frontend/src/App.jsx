2c50f36a42f32239a3a4a0105313edb28c0b5c2d
anhna <ana_mjl@hotmail.com>
2025-07-10 16:57:55 -0600

    Started base

--- blog-frontend/src/App.jsx
+++ blog-frontend/src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
+import NewPost from './pages/New Post/NewPost'
 
 function App() {
 
   return (
     <>
-      
+    <div className="app">
+      <main className='app-main'>
+        <div className='container'>
+          <NewPost />
+        </div>
+      </main>
+    </div>
+
+    <footer className='app-footer'>
+      <div className='container'>
+      <p>© 2025 Blog de Cesar Guerra. Todos los derechos reservados.</p>
+      </div>
+    </footer>
     </>
   )
 }
