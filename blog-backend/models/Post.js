aa3012d83083751e6e391ca046f5b859cd43dc98
anhna <ana_mjl@hotmail.com>
2025-07-28 16:59:44 -0600

    Add MVC: deleteOnePost

---
+++ blog-backend/models/Post.js
@@ -0,0 +1,52 @@
+// Datos iniciales en memoria
+import postsData from '../data/posts.json' with { type: 'json' }
+
+// CREATE
+export const createPost = (postData) => {
+    const newPost = {
+        id: postsData.length > 0 ? Math.max(...postsData.map(post => post.id)) + 1 : 1,
+        userId: postData.userId,
+        title: postData.title,
+        body: postData.body,
+        ...postData
+    }
+
+    postsData.push(newPost)
+    return newPost
+}
+
+// READ
+export const getPosts = () => {
+    return postsData
+}
+
+export const getPost = (id) => {
+    return postsData.find(post => post.id === parseInt(id))
+}
+
+// UPDATE
+export const updatePost = (id, postDataToUpdate) => {
+    const postIndex = postsData.findIndex(post => post.id === parseInt(id))
+    if (postIndex === -1) {
+        return null
+    }
+
+    const updatedPost = {
+        ...postsData[postIndex],
+        ...postDataToUpdate
+    }
+
+    postsData[postIndex] = updatedPost
+    return updatedPost
+}
+
+// DELETE
+export const deletePost = (id) => {
+    const postIndex = postsData.findIndex(post => post.id === parseInt(id))
+    if (postIndex === -1) {
+        return null
+    }
+
+    postsData.splice(postIndex, 1)
+    return true
+}
---
+++ blog-backend/routes/postRoutes.js
@@ -0,0 +1,18 @@
+import { Router } from 'express'
+import {
+  getAllPosts,
+  getPostById,
+  createOnePost,
+  updateOnePost,
+  deleteOnePost
+} from '../controllers/postController.js'
+
+const router = Router()
+
+router.get('/', getAllPosts)
+router.get('/:id', getPostById)
+router.post('/', createOnePost)
+router.patch('/:id', updateOnePost)
+router.delete('/:id', deleteOnePost)
+
+export default router
